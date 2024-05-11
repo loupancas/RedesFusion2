7599672492e9e8526e60efc542dc33c08fe774de
loupancas <129621626+loupancas@users.noreply.github.com>
2024-05-11 12:44:46 -0500

    Nuevos

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
 using static UnityEngine.ParticleSystem;
-using ParrelSync.NonCore;
+
 
 public class Player : NetworkBehaviour
 {
